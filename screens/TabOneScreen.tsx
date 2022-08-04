546c84e53eb231eaa42463973b7136f0f2d96cf7
Tim Eichholz <tim.eichholz98@web.de>
2022-08-04 22:16:02 +0200

    explore api

--- screens/TabOneScreen.tsx
+++ screens/TabOneScreen.tsx
@@ -1,14 +1,40 @@
-import { StyleSheet } from 'react-native';
+import React from 'react';
+import { StyleSheet, Button, FlatList, ListRenderItemInfo } from 'react-native';
 
 import EditScreenInfo from '../components/EditScreenInfo';
 import { Text, View } from '../components/Themed';
 import { RootTabScreenProps } from '../types';
+import { openDatabase } from 'expo-sqlite';
+
+type Transaction = {
+  name: string,
+  amount: number
+};
+
+type Transactions = Transaction[];
+
+const data: Transactions = [
+  { name: "Schuhe",
+    amount : 200.0 },
+  { name: "Eis",
+    amount : -200.0 },
+]
+
+const Item = (transaction: ListRenderItemInfo<Transaction>) => (
+  <View>
+    <Text style={styles.title}>{transaction.item.name}</Text>
+    <Text style={styles.title}>{transaction.item.amount}</Text>
+  </View>
+);
 
 export default function TabOneScreen({ navigation }: RootTabScreenProps<'TabOne'>) {
   return (
     <View style={styles.container}>
-      <Text style={styles.title}>Tab One</Text>
+      <Text style={styles.title}>Finanzübersicht</Text>
       <View style={styles.separator} lightColor="#eee" darkColor="rgba(255,255,255,0.1)" />
+      <FlatList data={data} renderItem={Item}/>
+      <Button title="+" />
+      <Button title="-" />
       <EditScreenInfo path="/screens/TabOneScreen.tsx" />
     </View>
   );
