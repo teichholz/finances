9fa42aa5284ad25ef1c6a4d29ec0b9c97de9a523
Tim Eichholz <tim.eichholz98@web.de>
2022-08-06 14:14:43 +0200

    add simple dialog and textInput

--- screens/TabOneScreen.tsx
+++ screens/TabOneScreen.tsx
@@ -1,9 +1,10 @@
-import React from 'react';
-import { StyleSheet, Button, FlatList, ListRenderItemInfo } from 'react-native';
+import React, { useState } from 'react';
+import { StyleSheet, Button, FlatList, ListRenderItemInfo, TextInput, SafeAreaView } from 'react-native';
 
 import EditScreenInfo from '../components/EditScreenInfo';
 import { Text, View } from '../components/Themed';
 import { RootTabScreenProps } from '../types';
+import { Dialog, } from '@rneui/themed';
 import { openDatabase } from 'expo-sqlite';
 
 type Transaction = {
@@ -27,14 +28,41 @@ const Item = (transaction: ListRenderItemInfo<Transaction>) => (
   </View>
 );
 
+
+
 export default function TabOneScreen({ navigation }: RootTabScreenProps<'TabOne'>) {
+  const [visible, setVisible] = useState(false);
+  const [amount, setAmount] = useState("0");
+
+  const toggleDialog = () => {
+    setVisible(!visible);
+  };
+
   return (
     <View style={styles.container}>
       <Text style={styles.title}>Finanzübersicht</Text>
       <View style={styles.separator} lightColor="#eee" darkColor="rgba(255,255,255,0.1)" />
       <FlatList data={data} renderItem={Item}/>
-      <Button title="+" />
-      <Button title="-" />
+      <View>
+        <View style={styles.fixToText}>
+          <Button title="+" onPress={toggleDialog} />
+          <Button title="-" onPress={toggleDialog} />
+        </View>
+      </View>
+      <Dialog
+        isVisible={visible}
+        onBackdropPress={toggleDialog}
+      >
+        <Dialog.Title title="Ausgabe hinzufügen"/>
+        <Text>Dialog body text. Add relevant information here.</Text>
+        <TextInput 
+           style={styles.textInput}
+           keyboardType='numeric'
+           onChangeText={setAmount}
+           value={amount}
+           maxLength={10}  //setting limit of input
+        />
+      </Dialog>
       <EditScreenInfo path="/screens/TabOneScreen.tsx" />
     </View>
   );
@@ -54,5 +82,15 @@ const styles = StyleSheet.create({
     marginVertical: 30,
     height: 1,
     width: '80%',
+  },
+  fixToText: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+  },
+  textInput: {
+    height: 40,
+    margin: 12,
+    borderWidth: 1,
+    padding: 10,
   },
 });
