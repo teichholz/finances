230eda4a0279587e080154f3322d16412f5a86b3
Tim Eichholz <tim.eichholz98@web.de>
2022-10-16 17:17:22 +0200

    check if transactions already were loaded

--- hooks/useDbTransactions.ts
+++ hooks/useDbTransactions.ts
@@ -1,6 +1,3 @@
-import { FontAwesome } from '@expo/vector-icons';
-import * as Font from 'expo-font';
-import * as SplashScreen from 'expo-splash-screen';
 import { useEffect, useState } from 'react';
 
 import * as SQLite from 'expo-sqlite';
@@ -14,22 +11,24 @@ export default function useDbTransactions() {
   const dispatch = useDispatch();
   const [transactionsLoaded, setTrasactionsLoaded] = useState<boolean>(false);
   useEffect(() => {
-    db.transaction((tx) => {
-      tx.executeSql(
-        `select * from transactions;`,
-        [],
-        (_: SQLite.SQLTransaction, resultSet: SQLite.SQLResultSet) => {
-          const rows = resultSet.rows._array;
-          rows.forEach((value) => dispatch(add(value)))
-          console.log("transactions loaded from database!");
-          setTrasactionsLoaded(true);
-        },
-        (transaction: SQLite.SQLTransaction, error: SQLite.SQLError) => {
-          console.log(error.message);
-          return true;
-        }
-      );
-    });
+    if (!transactionsLoaded) {
+      db.transaction((tx) => {
+        tx.executeSql(
+          `select * from transactions;`,
+          [],
+          (_: SQLite.SQLTransaction, resultSet: SQLite.SQLResultSet) => {
+            const rows = resultSet.rows._array;
+            rows.forEach((value) => dispatch(add(value)))
+            console.log("transactions loaded from database!");
+            setTrasactionsLoaded(true);
+          },
+          (transaction: SQLite.SQLTransaction, error: SQLite.SQLError) => {
+            console.log(error.message);
+            return true;
+          }
+        );
+      });
+    }
   }, []);
 
   return transactionsLoaded;
